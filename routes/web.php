c6a49d3de8e470069fdb4886c0a2fb02a47a3b25
Laura13294 <laura13294@hotmail.com>
2023-02-08 21:27:04 +0100

    pruebas vistas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::get('/{cadena}', function ($cadena) {
+
+    return view('welcome',[
+        'cadena'=>$cadena
+    ]);
+});*/
