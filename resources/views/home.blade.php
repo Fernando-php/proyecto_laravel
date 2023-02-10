bb70a2840afbfd08f78ce0d623b9e8a0db8cb4af
Laura13294 <laura13294@hotmail.com>
2023-02-10 13:40:35 +0100

    cambios pag principal y estilos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
     <title>{{ env('APP_NAME') }}</title>
     <link rel="stylesheet" href="{{ asset('css/style.css')}}" type="text/css">
     <link rel="icon" type="image/jpg" href="{{ asset('favicon.ico')}}"/>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
     <header>
@@ -18,10 +19,25 @@
         </nav>
     </header>
     <div id="container">
+        <h1>PÁGINA PRINCIPAL</h1>
+        <div id="content">
+            <div id="mensajes">
+                <h2>Mensajes</h2>
+            </div>
+            <div id="botones">
+                <form action="../../form-result.php" method="post">
+                    <input class="box" type="search" name="busquedamensajes" placeholder="Buscar mensajes...">
+                    <span class="icon"><button id="boton2"><i class="fa fa-search"></i></button></span>  
+                </form><br><br>
+                <form action="" method="post">
+                    <input id="boton" type="submit" value="Nuevo mensaje">
+                </form>
+            </div>
+        </div>
         
-        <h1>Página principal</h1>
     </div>
-    
-    
+    <footer>
+        <p>© 2023 Proyecto realizado por Ana Arenilla Villalva y Laura Sánchez Ayllón</p>
+    </footer>
 </body>
 </html>
