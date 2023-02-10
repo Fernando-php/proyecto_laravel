2677ef166c5d89a322b35bf23dc0d7f74ad1e9c5
Laura13294 <laura13294@hotmail.com>
2023-02-10 12:25:52 +0100

    pruebas estilos, favicon e imagenes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }}</title>
     <link rel="stylesheet" href="{{ asset('css/style.css')}}" type="text/css">
-    
+    <link rel="icon" type="image/jpg" href="{{ asset('favicon.ico')}}"/>
 </head>
 <body>
-
-    <h1>Página principal</h1>
+    <header>
+        <img id="logo" src="{{ asset('img/logo.png')}}" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="">HOME</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div id="container">
+        
+        <h1>Página principal</h1>
+    </div>
+    
     
 </body>
 </html>
