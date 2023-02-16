a7f1db708ce5bbc9064da688d5667ae12b1187eb
Laura13294 <laura13294@hotmail.com>
2023-02-16 15:02:50 +0100

    categorias mensajes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Categoria;
 use App\Models\Mensaje;
+use App\Models\Users;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,16 @@
             @foreach ($mensajes as $mensaje)
             <div id="tituloCategoria">
                 <h3>{{$mensaje->titulo}}</h3>
+                @foreach ($categorias as $categoria)
                 {{-- <h4>{{$mensaje->categoria_id}}</h4>  ¿Cómo poner el nombre de la categoría en vez del id??? --}}
+                    @if ($categoria->id == $mensaje->categoria_id)
+                    <h4>{{$categoria->nombre}}</h4>
+                    @endif
+                @endforeach
             </div>
+                {{-- <img src="" alt=""> Poner avatar del usuario --}}
                 <p>{{$mensaje->descripcion}}</p>
+                {{-- <h4>{{$mensaje->user_id}}</h4>  ¿Cómo poner el nombre del usuario en vez del id??? --}}
             @endforeach
             <div id="paginacion">
                 {{-- Paginación --}}
